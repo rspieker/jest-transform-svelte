6980232d0a351832606d122cf8a1edb9a3a01fdd
Rogier Spieker <rogier@konfirm.eu>
2019-06-02 21:00:12 +0200

    Added clicks, waiting for all tests to complete (resolves #3)
    Added event example
    Improved tests to ensure the proper types (strings, not numbers) are verified and signal when the test is actually completed.

--- example/source/Sample.svelte
+++ example/source/Sample.svelte
@@ -1,8 +1,17 @@
-<span data-length="{ length }">{ text }</span>
+<span on:click="{ updateClicks }" data-clicks="{ clicks }" data-length="{ length }">{ text }</span>
+
+<svelte:options accessors="{ true }" />
 
 <script>
+	//  internal click counter
+	let clicks = 0;
 	//  allow for text overrides
 	export let text = '';
+
+	//  handler for the click event
+	function updateClicks() {
+		++clicks;
+	}
 
 	//  "computed" text length
 	$: length = text.length;
--- example/test/Sample.spec.js
+++ example/test/Sample.spec.js
@@ -1,7 +1,7 @@
 import Sample from '../source/Sample.svelte';
 
 describe('Sample', () => {
-	it('is empty by default', () => {
+	it('is empty by default', (next) => {
 		//  create a new element
 		const target = document.createElement('div');
 		//  render the component in the new element
@@ -12,26 +12,47 @@ describe('Sample', () => {
 
 			//  test rendered element
 			expect(element.textContent).toBe('');
-			expect(element.getAttribute('data-length')).toBe(0);
+			expect(element.getAttribute('data-length')).toBe('0');
 
 			//  test component
-			expect(sample.test).toBe('');
-			expect(sample.length).toBe(0);
+			expect(sample.text).toBe('');
 
 			sample.text = 'A foo walks into a bar';
 
 			setTimeout(() => {
 				expect(element.textContent).toBe('A foo walks into a bar');
-				expect(element.getAttribute('data-length')).toBe(22);
+				expect(element.getAttribute('data-length')).toBe('22');
+
+				next();
 			});
+		}, 10);
+	});
+
+	it('clicks', (next) => {
+		//  create a new element
+		const target = document.createElement('div');
+		//  render the component in the new element
+		const sample = new Sample({ target });
+
+		setTimeout(() => {
+			const { firstElementChild: element } = target;
+
+			expect(element.getAttribute('data-clicks')).toBe('0');
+			element.click();
+
+			setTimeout(() => {
+				expect(element.getAttribute('data-clicks')).toBe('1');
+
+				next();
+			}, 10);
 		}, 10);
 	});
 
 	it.each`
 		text         | length
-		${'foo'}     | ${3}
-		${'bar-baz'} | ${7}
-		${'sample'}  | ${5}
+		${'foo'}     | ${'3'}
+		${'bar-baz'} | ${'7'}
+		${'sample'}  | ${'5'}
 	`('$text renders and has length $length', ({ text, length }) => {
 		//  create a new element
 		const target = document.createElement('div');
